3b82272e01bd583d48692e26cd858c2ac9a0a657
Ajay-Kumar-2123 <ddarn3681@gmail.com>
2025-04-03 10:32:12 +0530

    Update train.py

--- train.py
+++ train.py
@@ -67,6 +67,6 @@ def upload():
 
     return jsonify(response)
 
-if __name__ == "__main__":  
-    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT not set  
-    app.run(host="0.0.0.0", port=port) 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no environment variable
+    app.run(debug=True, host="0.0.0.0", port=port)
